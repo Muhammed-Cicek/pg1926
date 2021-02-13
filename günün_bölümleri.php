f74e2d76c5d30fa5c20305f54c54c50f9f4395dc
Muhammed-Cicek <79014212+Muhammed-Cicek@users.noreply.github.com>
2021-02-13 20:57:58 +0300

    Create günün_bölümleri.php

---
+++ günün_bölümleri.php
@@ -0,0 +1,37 @@
+<?php
+
+date_default_timezone_set('Europe/Istanbul');
+
+$saat = date("H");
+$ilk_vakit = "06";
+$ogle  = "10";
+$orta_vakit = "17";
+$aksam = "20";
+$yatsi= "00";
+
+if($saat >= $ilk_vakit AND $saat < $ogle)
+{
+    echo "Günaydın";
+}
+
+else if($saat >= $ogle AND $saat < $orta_vakit )
+{
+    echo "İyi Günler";
+}
+
+else if($saat >= $orta_vakit AND $saat < $aksam)
+{
+    echo "İyi Akşamlar";
+}
+
+else if($saat >= $aksam && $saat < $yatsi)
+{
+    echo "İyi Geceler";
+}
+
+else
+{
+    echo "Esenlikler Dilerim";
+}
+
+?> 
